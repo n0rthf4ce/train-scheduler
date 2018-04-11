9b7d71c57e458af16c2cb226fc18df62c2056582
n0rthf4ce <eric.liu85@gmail.com>
2018-04-11 13:10:49 -0700

    change authentication to new page

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -11,13 +11,15 @@ var config = {
 firebase.initializeApp(config);
 
 var provider = new firebase.auth.GoogleAuthProvider();
-
-firebase.auth().signInWithPopup(provider).then(function(result) {
-    // This gives you a Google Access Token. You can use it to access the Google API.
-    var token = result.credential.accessToken;
+firebase.auth().signInWithRedirect(provider);
+firebase.auth().getRedirectResult().then(function(result) {
+    if (result.credential) {
+      // This gives you a Google Access Token. You can use it to access the Google API.
+      var token = result.credential.accessToken;
+      // ...
+    }
     // The signed-in user info.
     var user = result.user;
-    // ...
   }).catch(function(error) {
     // Handle Errors here.
     var errorCode = error.code;
